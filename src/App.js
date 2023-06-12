1a7642fd3e08c1149225adf6bdd93da9307e086c
nehuenV97 <veganehuen97@gmail.com>
2023-06-12 11:31:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Typography>
+          Hola soy React Js
+        </Typography>
         <a
           className="App-link"
           href="https://reactjs.org"
